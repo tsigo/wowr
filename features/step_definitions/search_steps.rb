382022ab174e2ceb2cf209f9ef4f6d17e3d66dda
rspeicher <rspeicher@gmail.com>
2010-10-14 11:20:47 -0400

    Add arena team and item search features

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,18 +1,17 @@
-When /^I search for a character named "([^"]+)"$/ do |name|
-  FakeWeb.register_uri(:get, /searchQuery=#{name}/, :body => file_fixture("character_search_#{name.downcase}.xml"))
-  @results = Wowr::API.new.search_characters(name)
-end
+When /^I search for an? (.+) named "([^"]+)"$/ do |type, name|
+  # Hacky pluralization; got lucky with our types
+  search_method = "search_#{(type + 's').downcase.gsub(/\s+/, '_')}"
+  fixture_path  = type.downcase.gsub(/\s+/, '_')
 
-When /^I search for a guild named "([^"]+)"$/ do |name|
-  FakeWeb.register_uri(:get, /searchQuery=#{name}/, :body => file_fixture("guild_search_#{name.downcase}.xml"))
-  @results = Wowr::API.new.search_guilds(name)
+  FakeWeb.register_uri(:get, /searchQuery=#{name}/, :body => file_fixture("#{fixture_path}_search_#{name.downcase}.xml"))
+  @results = Wowr::API.new.send(search_method, name)
 end
 
 Then /^I should see the following YAML:$/ do |output|
   @results.to_yaml.to_s.should match(output)
 end
 
-Then /^I should see the following result:$/ do |table|
+Then /^I should see the following as the first result:$/ do |table|
   table.raw.each do |attr, val|
     @results[0].send(attr).to_s.should eql(val)
   end
