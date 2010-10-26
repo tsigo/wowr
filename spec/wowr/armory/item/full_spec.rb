07a4e3799e3afa79942443cc568a57b5a87c306a
rspeicher <rspeicher@gmail.com>
2010-10-26 17:40:37 -0400

    Item::Full spec

--- spec/wowr/armory/item/full_spec.rb
+++ spec/wowr/armory/item/full_spec.rb
@@ -2,6 +2,34 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe Full do
-    it { pending }
+    subject { Full.new(fake_info, fake_tooltip) }
+
+    it "should delegate info methods to Info" do
+      Info.any_instance.expects(:info_method).returns('')
+      expect { subject.info_method }.to_not raise_error
+    end
+
+    it "should delegate tooltip methods to Tooltip" do
+      Tooltip.any_instance.expects(:tooltip_method).returns('')
+      expect { subject.tooltip_method }.to_not raise_error
+    end
+
+    it "should raise NoMethodError when given an invalid method" do
+      expect { subject.foo }.to raise_error(NoMethodError, /undefined method 'foo'/)
+    end
+
+    protected
+
+    def fake_info
+      xml = file_fixture('armory/item-info/40395.xml')
+
+      (Hpricot.XML(xml)%'itemInfo')
+    end
+
+    def fake_tooltip
+      xml = file_fixture('armory/item-tooltip/40395.xml')
+
+      (Hpricot.XML(xml)%'itemTooltip')
+    end
   end
 end
