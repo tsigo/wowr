6c9aeac2159d3103770e3c67e444fe75716e8e41
rspeicher <rspeicher@gmail.com>
2010-10-26 15:32:14 -0400

    Item::Quest spec

--- lib/wowr/armory/item/quest.rb
+++ lib/wowr/armory/item/quest.rb
@@ -1,21 +1,17 @@
 module Wowr
   module Armory
     module Item
-      # <rewardFromQuests>
-      #   <quest name="Justice Dispensed" level="39" reqMinLevel="30" id="11206" area="Dustwallow Marsh" suggestedPartySize="0"></quest>
-      #   <quest name="Peace at Last" level="39" reqMinLevel="30" id="11152" area="Dustwallow Marsh" suggestedPartySize="0"></quest>
-      # </rewardFromQuests>
       # TODO: Rename
       class Quest
         attr_reader :name, :id, :level, :min_level, :area, :suggested_party_size
 
         def initialize(elem)
-          @name       = elem[:name]
-          @id         = elem[:id].to_i
-          @level      = elem[:level].to_i
-          @min_level  = elem[:min_level].to_i
-          @area       = elem[:area]
-          @suggested_party_size = elem[:suggested_party_size].to_i
+          @name                 = elem[:name]
+          @id                   = elem[:id].to_i
+          @level                = elem[:level].to_i
+          @min_level            = elem[:reqMinLevel].to_i
+          @area                 = elem[:area]
+          @suggested_party_size = elem[:suggestedPartySize].to_i
         end
       end
     end
--- spec/wowr/armory/item/quest_spec.rb
+++ spec/wowr/armory/item/quest_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe Quest do
-    it { pending }
+    subject { Quest.new(fake_element) }
+
+    its(:name)                 { should eql("Battle at Valhalas: Final Challenge") }
+    its(:id)                   { should eql(13219) }
+    its(:level)                { should eql(80) }
+    its(:min_level)            { should eql(77) }
+    its(:area)                 { should eql("Icecrown") }
+    its(:suggested_party_size) { should eql(5) }
+
+    protected
+
+    def fake_element
+      # http://www.wowarmory.com/item-info.xml?i=44382
+      xml = <<-XML
+      <rewardFromQuests>
+        <quest area="Icecrown" id="13219" level="80" name="Battle at Valhalas: Final Challenge" reqMinLevel="77" suggestedPartySize="5" type="Group"/>
+      </rewardFromQuests>
+      XML
+
+      (Hpricot.XML(xml)%'rewardFromQuests'/'quest')[0]
+    end
   end
 end
