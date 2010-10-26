fff485478ebc91149e18c5069c66285311a1ca4e
rspeicher <rspeicher@gmail.com>
2010-10-26 14:55:18 -0400

    Change fake_element definition for Item::SetData spec

--- spec/wowr/armory/item/set_data_spec.rb
+++ spec/wowr/armory/item/set_data_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe SetData do
-    subject { SetData.new(Hpricot.XML(fake_element)) }
+    subject { SetData.new(fake_element) }
 
     its(:name) { should eql("Sanctification Garb") }
     its(:items) { should be_an(Array) }
@@ -28,7 +28,7 @@ module Wowr::Armory::Item
     protected
 
     def fake_element
-      <<-XML
+      xml = <<-XML
       <name>Sanctification Garb</name>
       <item name="Circlet of Sanctification"/>
       <item name="Handwraps of Sanctification"/>
@@ -38,6 +38,8 @@ module Wowr::Armory::Item
       <setBonus desc="Increases the damage done by your Devouring Plague by 15%." threshold="2"/>
       <setBonus desc="Your Mind Blast also grants you 240 haste rating for 4 sec." threshold="4"/>
       XML
+
+      Hpricot.XML(xml)
     end
   end
 end
