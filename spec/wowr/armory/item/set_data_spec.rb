3c1f672e58a5238deec5affdc9f17d7315ce09c9
rspeicher <rspeicher@gmail.com>
2010-10-26 14:35:51 -0400

    Item::SetData spec

--- spec/wowr/armory/item/set_data_spec.rb
+++ spec/wowr/armory/item/set_data_spec.rb
@@ -2,6 +2,42 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe SetData do
-    it { pending }
+    subject { SetData.new(Hpricot.XML(fake_element)) }
+
+    its(:name) { should eql("Sanctification Garb") }
+    its(:items) { should be_an(Array) }
+    its(:set_bonuses) { should be_an(Array) }
+
+    # Alias methods
+    its(:to_s) { should eql("Sanctification Garb") }
+
+    it "should parse items in the set" do
+      subject.items.length.should eql(5)
+    end
+
+    describe "set bonuses" do
+      it "should collect all bonuses" do
+        subject.set_bonuses.length.should eql(2)
+      end
+
+      it "should be an array of SetBonus instances" do
+        subject.set_bonuses[0].should be_kind_of(SetBonus)
+      end
+    end
+
+    protected
+
+    def fake_element
+      <<-XML
+      <name>Sanctification Garb</name>
+      <item name="Circlet of Sanctification"/>
+      <item name="Handwraps of Sanctification"/>
+      <item name="Mantle of Sanctification"/>
+      <item name="Pants of Sanctification"/>
+      <item name="Raiments of Sanctification"/>
+      <setBonus desc="Increases the damage done by your Devouring Plague by 15%." threshold="2"/>
+      <setBonus desc="Your Mind Blast also grants you 240 haste rating for 4 sec." threshold="4"/>
+      XML
+    end
   end
 end
