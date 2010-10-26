c5fd35f3294828f857eb0efeff745ff57ccb860b
rspeicher <rspeicher@gmail.com>
2010-10-26 13:31:34 -0400

    Item::SetBonus spec

--- spec/wowr/armory/item/set_bonus_spec.rb
+++ spec/wowr/armory/item/set_bonus_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe SetBonus do
-    it { pending }
+    subject { SetBonus.new(fake_element) }
+
+    its(:threshold)   { should eql(1) }
+    its(:description) { should eql("Set bonus description") }
+
+    # Alias methods
+    its(:desc) { should eql("Set bonus description") }
+    its(:to_s) { should eql("Set bonus description") }
+
+    protected
+
+    def fake_element
+      { :threshold => 1, :desc => "Set bonus description" }
+    end
   end
 end
