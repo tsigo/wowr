05304c648119e55c4a6f9fbd29b45c8b53583083
rspeicher <rspeicher@gmail.com>
2010-10-30 16:40:40 -0400

    GuildBank::Base was missing its superclass

--- lib/wowr/armory/guild_bank/base.rb
+++ lib/wowr/armory/guild_bank/base.rb
@@ -1,7 +1,7 @@
 module Wowr
   module Armory
     module GuildBank
-      class Base
+      class Base < Wowr::Armory::Guild::Base
         attr_reader :motd, :info, :money, :ranks, :bags
 
         def initialize(elem)
@@ -13,7 +13,7 @@ module Wowr
 
             @bags = []
           (elem%'bags'/:bag).each do |bag|
-            @bags[bag[:id].to_i] = GuildBankBag.new(bag)
+            @bags[bag[:id].to_i] = Bag.new(bag)
           end
 
           @ranks = []
