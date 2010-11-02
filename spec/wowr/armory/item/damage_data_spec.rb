da1efe88da48a0bfcb9bee55d7bb1dd05a65cfc7
rspeicher <rspeicher@gmail.com>
2010-11-02 14:07:20 -0400

    Item::DamageData spec

--- spec/wowr/armory/item/damage_data_spec.rb
+++ spec/wowr/armory/item/damage_data_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe DamageData do
-    it { pending }
+    subject { DamageData.new(fake_element) }
+
+    its(:type)  { should eql(0) }
+    its(:min)   { should eql(886) }
+    its(:max)   { should eql(1329) }
+    its(:speed) { should eql(3.4) }
+    its(:dps)   { should eql(325.74) }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/item-tooltip/50727.xml')
+      (Hpricot.XML(xml)%'damageData')
+    end
   end
 end
