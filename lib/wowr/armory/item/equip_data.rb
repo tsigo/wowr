d54a4db7fcf08918372324848e26f514c4105994
rspeicher <rspeicher@gmail.com>
2010-11-02 13:33:36 -0400

    Item::EquipData spec

--- lib/wowr/armory/item/equip_data.rb
+++ lib/wowr/armory/item/equip_data.rb
@@ -1,8 +1,33 @@
 module Wowr
   module Armory
     module Item
+      # Represents an <tt><equipData></tt> element
+      #
+      # == Relevant XML example:
+      #
+      #   <equipData>
+      #     <inventoryType>18</inventoryType>
+      #     <containerSlots>22</containerSlots>
+      #     <subclassName>Bag</subclassName>
+      #   </equipData>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/item-tooltip.xml?i=40395
+      # * http://www.wowarmory.com/item-tooltip.xml?i=49295
       class EquipData
-        attr_reader :inventory_type, :subclass_name, :container_slots
+        # @return [Integer]
+        attr_reader :inventory_type
+
+        # @example
+        #   "Bag"
+        #   "Mace"
+        # @return [String]
+        attr_reader :subclass_name
+
+        # For bags, the number of slots
+        # @return [Integer]
+        attr_reader :container_slots
 
         def initialize(elem)
           @inventory_type = (elem%'inventoryType').html.to_i
--- spec/wowr/armory/item/equip_data_spec.rb
+++ spec/wowr/armory/item/equip_data_spec.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe EquipData do
-    it { pending }
+    subject { EquipData.new(fake_element) }
+
+    its(:inventory_type)  { should eql(18) }
+    its(:subclass_name)   { should eql('Bag') }
+    its(:container_slots) { should eql(22) }
+
+    protected
+
+    def fake_element
+      # http://www.wowarmory.com/item-tooltip.xml?i=49295
+      xml = <<-XML
+      <equipData>
+        <inventoryType>18</inventoryType>
+        <containerSlots>22</containerSlots>
+        <subclassName>Bag</subclassName>
+      </equipData>
+      XML
+
+      (Hpricot.XML(xml)%'equipData')
+    end
   end
 end
