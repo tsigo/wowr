0e853e1bded43677479f4918cf56eb2d2de8bc16
rspeicher <rspeicher@gmail.com>
2010-11-02 14:21:49 -0400

    Item::Search spec

--- lib/wowr/armory/item/search.rb
+++ lib/wowr/armory/item/search.rb
@@ -1,10 +1,51 @@
 module Wowr
   module Armory
     module Item
-      # A really basic item type returned by searches
+      # Represents a single <tt><item></tt> element returned by an item search
+      #
+      # == Relevant XML example:
+      #
+      #   <item icon="inv_boots_plate_09" id="45218" name="Blood-Caked Stompers" rarity="3" url="i=45218">
+      #     <filter name="itemLevel" value="200"/>
+      #     <filter name="source" value="sourceType.vendor"/>
+      #     <filter name="relevance" value="100"/>
+      #   </item>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/search.xml?searchQuery=cake&searchType=items
       class Search < Wowr::Armory::Item::Base
-        attr_reader :url, :rarity,
-          :source, :item_level, :relevance
+        # URL parameters for the item
+        # @example
+        #   "i=45218"
+        # @return [String]
+        attr_reader :url
+
+        # Item rarity (quality)
+        # * 0 - Trash (Grey)
+        # * 1 - Common (White)
+        # * 2 - Uncommon (Green)
+        # * 3 - Rare (Blue)
+        # * 4 - Epic (Purple)
+        # * 5 - Legendary (Orange)
+        # * 6
+        # * 7 - Heirloom (Gold)
+        attr_reader :rarity
+
+        # sourceType string
+        # @example
+        #   "sourceType.vendor"
+        #   "sourceType.creature"
+        # @return [String]
+        attr_reader :source
+
+        # @return [Integer]
+        attr_reader :item_level
+
+        # Search relevance
+        # @return [Integer]
+        attr_reader :relevance
+
         alias_method :level, :item_level
 
         def initialize(elem, api = nil)
--- spec/wowr/armory/item/search_spec.rb
+++ spec/wowr/armory/item/search_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 module Wowr::Armory::Item
   describe Search do
-    it { pending }
+    subject { Search.new(fake_element) }
+
+    its(:url)         { should eql("i=45218") }
+    its(:rarity)      { should eql(3) }
+    its(:source)      { should eql('sourceType.vendor') }
+    its(:item_level)  { should eql(200) }
+    its(:relevance)   { should satisfy { |n| n > 0 && n <= 100 } }
+
+    # Aliased methods
+    its(:level) { should eql(200) }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/search/items_cake.xml')
+      (Hpricot.XML(xml)%'items'/'item')[0]
+    end
   end
 end
