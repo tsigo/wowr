844ebc4fe1c376bc707bbf5860564e5029385c16
rspeicher <rspeicher@gmail.com>
2010-11-07 17:16:07 -0500

    Move FileFixtures module to spec/support directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,7 @@ require 'wowr'
 require 'fakeweb'
 FakeWeb.allow_net_connect = false
 
-module FileFixtures
-  def file_fixture(*args)
-    File.read(File.expand_path("../file_fixtures/#{File.join(args)}", __FILE__))
-  end
-end
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :mocha
