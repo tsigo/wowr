69073757825bf65de05cdaf9bc6456f9a410df27
rspeicher <rspeicher@gmail.com>
2010-11-08 15:30:21 -0500

    Spec ArenaTeam::Emblem

--- lib/wowr/armory/arena_team/emblem.rb
+++ lib/wowr/armory/arena_team/emblem.rb
@@ -1,10 +1,30 @@
 module Wowr
   module Armory
     module ArenaTeam
-      # An arena team's logo
+      # = Emblem
+      #
+      # Represents an arena team's <tt>emblem</tt> element
+      #
+      # == Relevant XML example:
+      #
+      #   <emblem background="ff00b825" borderColor="ff240002" borderStyle="6" iconColor="ff001440" iconStyle="82"/>
       class Emblem
-        attr_reader :background, :border_color, :border_style, :icon_colour, :icon_style
+        # @return [String]
+        attr_reader :background
 
+        # @return [String]
+        attr_reader :border_color
+
+        # @return [Integer]
+        attr_reader :border_style
+
+        # @return [String]
+        attr_reader :icon_color
+
+        # @return [Integer]
+        attr_reader :icon_style
+
+        # @param [Hpricot::Elem] elem <tt>emblem</tt> element
         def initialize(elem)
           @background   = elem[:background]
           @border_color = elem[:borderColor]
--- spec/wowr/armory/arena_team/emblem_spec.rb
+++ spec/wowr/armory/arena_team/emblem_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 module Wowr::Armory::ArenaTeam
   describe Emblem do
-    it { pending }
+    subject { Emblem.new(fake_element) }
+
+    its(:background)   { should eql("ff00b825") }
+    its(:border_color) { should eql("ff240002") }
+    its(:border_style) { should eql(6) }
+    its(:icon_color)   { should eql("ff001440") }
+    its(:icon_style)   { should eql(82) }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/team-info/dont_kill_comfort_ysera.xml')
+      (Hpricot.XML(xml)%'emblem')
+    end
   end
 end
