8549692108158f18ac46a51e3d30cfcff19fec27
rspeicher <rspeicher@gmail.com>
2010-11-08 17:00:22 -0500

    Spec Character::Skill
    Only(?!) 50 pendings remaining!

--- lib/wowr/armory/character/skill.rb
+++ lib/wowr/armory/character/skill.rb
@@ -1,12 +1,51 @@
 module Wowr
   module Armory
     module Character
-      # eg Daggers, Riding, Fishing, language
+      # = Skill
+      #
+      # Represents a <tt>skill</tt> element
+      #
+      # == Relevant XML examples:
+      #
+      #   <skill id="202" key="engineering" max="450" name="Engineering" value="450"/>
+      #
+      #   <skill id="755" key="jewelcrafting" max="450" name="Jewelcrafting" value="450"/>
+      #
+      #   <skill id="185" key="cooking" max="450" name="Cooking" value="450"/>
+      #
+      #   <skill id="129" key="firstaid" max="450" name="First Aid" value="450"/>
+      #
+      #   <skill id="356" key="fishing" max="375" name="Fishing" value="302"/>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/character-sheet.xml?r=Mal'Ganis&cn=Alephone
+      # * http://www.wowarmory.com/character-sheet.xml?r=Mal'Ganis&cn=Sebudai
       class Skill
-        attr_reader :key, :name, :value, :max
+        # @example
+        #   "enchanting"
+        #   "firstaid"
+        # @return [String]
+        attr_reader :key
+
+        # @example
+        #   "Enchanting"
+        #   "First Aid"
+        # @return [String]
+        attr_reader :name
+
+        # Current skill level
+        # @return [Integer]
+        attr_reader :value
+
+        # Maximum skill level
+        # @return [Integer]
+        attr_reader :max
+
         alias_method :to_s, :name
         alias_method :to_i, :value
 
+        # @param [Hpricot::Elem] elem <tt>skill</tt> element
         def initialize(elem)
           @key    = elem[:key]
           @name   = elem[:name]
--- spec/wowr/armory/character/skill_spec.rb
+++ spec/wowr/armory/character/skill_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 module Wowr::Armory::Character
   describe Skill do
+    subject { Skill.new(fake_element) }
 
-    it { pending }
+    its(:key)   { should eql('firstaid') }
+    its(:name)  { should eql('First Aid') }
+    its(:value) { should eql(450) }
+    its(:max)   { should eql(450) }
 
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-sheet/sebudai_mal_ganis.xml')
+      (Hpricot.XML(xml).search("skill[@id=129]").first)
+    end
   end
 end
