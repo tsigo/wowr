ed62805f1f470cc2cbe5bf11fe4c0f7764da2d97
rspeicher <rspeicher@gmail.com>
2010-11-08 15:45:01 -0500

    Spec Character::Defenses

--- lib/wowr/armory/character/defenses.rb
+++ lib/wowr/armory/character/defenses.rb
@@ -1,9 +1,45 @@
 module Wowr
   module Armory
     module Character
+      # = Defenses
+      #
+      # Represents a <tt>defenses</tt> element group
+      #
+      # == Relevant XML example:
+      #
+      #   <defenses>
+      #     <armor base="21842" effective="21842" percent="58.91" petBonus="-1"/>
+      #     <defense decreasePercent="0.00" increasePercent="0.00" plusDefense="0" rating="0" value="400.00"/>
+      #     <dodge increasePercent="0.00" percent="5.82" rating="0"/>
+      #     <parry increasePercent="0.11" percent="5.12" rating="5"/>
+      #     <block increasePercent="0.00" percent="5.00" rating="0"/>
+      #     <resilience damagePercent="0" hitPercent="0" value="0.00"/>
+      #   </defenses>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/character-sheet.xml?r=Mal'Ganis&cn=Alephone
+      # * http://www.wowarmory.com/character-sheet.xml?r=Mal'Ganis&cn=Sebudai
       class Defenses
-        attr_reader :armor, :defense, :dodge, :parry, :block, :resilience
+        # @return [DefenseStats::Armor]
+        attr_reader :armor
 
+        # @return [DefenseStats::Defense]
+        attr_reader :defense
+
+        # @return [DefenseStats::Avoidance]
+        attr_reader :dodge
+
+        # @return [DefenseStats::Avoidance]
+        attr_reader :parry
+
+        # @return [DefenseStats::Avoidance]
+        attr_reader :block
+
+        # @return [DefenseStats::Resilience]
+        attr_reader :resilience
+
+        # @param [Hpricot::Elem] elem <tt>defenses</tt> element
         def initialize(elem)
           @armor      = DefenseStats::Armor.new(elem%'armor')
           @defense    = DefenseStats::Defense.new(elem%'defense')
--- spec/wowr/armory/character/defenses_spec.rb
+++ spec/wowr/armory/character/defenses_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 module Wowr::Armory::Character
   describe Defenses do
-    it { pending }
+    subject { Defenses.new(fake_element) }
+
+    its(:armor)      { should be_kind_of(DefenseStats::Armor) }
+    its(:defense)    { should be_kind_of(DefenseStats::Defense) }
+    its(:dodge)      { should be_kind_of(DefenseStats::Avoidance) }
+    its(:parry)      { should be_kind_of(DefenseStats::Avoidance) }
+    its(:block)      { should be_kind_of(DefenseStats::Avoidance) }
+    its(:resilience) { should be_kind_of(DefenseStats::Resilience) }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-sheet/alephone_mal_ganis.xml')
+      (Hpricot.XML(xml)%'defenses')
+    end
   end
 end
