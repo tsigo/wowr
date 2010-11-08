346920a77d280e5bceb7d8b3b956f540276a2218
rspeicher <rspeicher@gmail.com>
2010-11-08 16:12:56 -0500

    Spec Character::TalentSpec

--- lib/wowr/armory/character/talent_spec.rb
+++ lib/wowr/armory/character/talent_spec.rb
@@ -1,19 +1,56 @@
 module Wowr
   module Armory
     module Character
-      # Note the list of talent trees starts at 1. This is quirky, but that's what's used in the XML
+      # = TalentSpec
+      #
+      # Represents a <tt>talentGroup</tt> element
+      #
+      # == Relevant XML example:
+      #
+      #   <talentGroup active="1" group="2" icon="ability_hunter_focusedaim" prim="Marksmanship">
+      #     <talentSpec treeOne="0" treeThree="5" treeTwo="31" value="0000000000000000000230231103212023122103200000000000000000"/>
+      #     <glyphs/>
+      #   </talentGroup>
+      #
+      # @todo Rename? It's a Talent Group, not just a Talent Spec
       class TalentSpec
-        attr_reader :trees, :active, :group, :primary, :point_distribution
+        # An array containing the total points in each tree.
+        # @note Tree one is in array index 0, two is in 1, and three is in 2
+        # @return [Array]
+        attr_reader :trees
 
+        # Whether or not the spec is currently active
+        # @return [Boolean]
+        attr_reader :active
+
+        # Either the first or second talent spec
+        # @return [Integer]
+        attr_reader :group
+
+        # The primary tree (i.e., the one with the most points)
+        # @example
+        #   "Discipline"
+        #   "Holy"
+        #   "Shadow"
+        # @return [String]
+        attr_reader :primary
+
+        # The number of points in each talent, left-to-right, top-to-bottom
+        # @example No points in the first tree, a majority in the second and five total in the third
+        #   "0000000000000000000230231103212023122103200000000000000000"
+        # @return [String]
+        attr_reader :point_distribution
+
+        # @param [Hpricot::Elem] elem <tt>talentGroup</tt> element
         def initialize(elem)
-          tree_elem = elem%'talentSpec'
-          @trees = []
-          @trees[0] = tree_elem[:treeOne].to_i
-          @trees[1] = tree_elem[:treeTwo].to_i
-          @trees[2] = tree_elem[:treeThree].to_i
-          @active = (elem[:active].to_i == 1 ? true : false)
-          @group = elem[:group].to_i
-          @primary = elem[:prim]
+          tree_elem           = elem%'talentSpec'
+          @trees              = []
+          @trees[0]           = tree_elem[:treeOne].to_i
+          @trees[1]           = tree_elem[:treeTwo].to_i
+          @trees[2]           = tree_elem[:treeThree].to_i
+          @active             = (elem[:active].to_i == 1 ? true : false)
+          @group              = elem[:group].to_i
+          @primary            = elem[:prim]
           @point_distribution = tree_elem[:value]
         end
       end
--- spec/wowr/armory/character/talent_spec_spec.rb
+++ spec/wowr/armory/character/talent_spec_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 module Wowr::Armory::Character
   describe TalentSpec do
+    subject { TalentSpec.new(fake_element) }
 
-    it { pending }
+    its(:active) { should be_true }
+    its(:group) { should eql(2) }
+    its(:primary) { should eql("Marksmanship") }
+    its(:point_distribution) { should match(/^\d+$/) }
 
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-talents/sebudai_mal_ganis.xml')
+      (Hpricot.XML(xml)%'talentGroup')
+    end
   end
 end
