dd3fdb86ec46258f99e5d2c25237cf4fc4c21bdd
rspeicher <rspeicher@gmail.com>
2010-11-08 00:25:14 -0500

    Spec ArenaTeam::Base

--- lib/wowr/armory/arena_team/base.rb
+++ lib/wowr/armory/arena_team/base.rb
@@ -1,37 +1,97 @@
 module Wowr
   module Armory
     module ArenaTeam
+      # = Base
+      #
+      # Represents an <tt>areanTeam</tt> element from multiple sources
+      #
+      # == Relevant XML example:
+      #
+      #   <arenaTeam battleGroup="Cyclone" created="1231545600000"
+      #     faction="Horde" factionId="1" gamesPlayed="0" gamesWon="0"
+      #     lastSeasonRanking="0" name="LEMON" ranking="0" rating="0"
+      #     realm="Stormscale" realmUrl="b=Cyclone&amp;r=Stormscale&amp;ts=2&amp;t=LEMON&amp;ff=realm&amp;fv=Stormscale&amp;select=LEMON"
+      #     relevance="100" season="0" seasonGamesPlayed="0" seasonGamesWon="0"
+      #     size="2" teamSize="2" teamUrl="r=Stormscale&amp;ts=2&amp;t=LEMON&amp;select=LEMON"
+      #     url="r=Stormscale&amp;ts=2&amp;t=LEMON&amp;select=LEMON">
       class Base
-        attr_reader :name, :size, :battle_group, :faction, :faction_id, :realm, :realm_url,
-          :games_played, :games_won, :ranking, :rating,
-          :season_games_played, :season_games_won, :last_season_ranking,
-          :relevance, :url, :url_escape,
-          :characters,  # can be blank on search results
-          :emblem
+        # @return [String]
+        attr_reader :name
+
+        # @return [Integer]
+        attr_reader :size
+
+        # @return [String]
+        attr_reader :battle_group
+
+        # @return [String]
+        attr_reader :faction
+
+        # Faction ID:
+        # * 0 - Alliance
+        # * 1 - Horde
+        # @return [Integer]
+        attr_reader :faction_id
+
+        # @return [String]
+        attr_reader :realm
+
+        # @return [String]
+        attr_reader :realm_url
+
+        # @return [Integer]
+        attr_reader :games_played
+
+        # @return [Integer]
+        attr_reader :games_won
+
+        # @return [Integer]
+        attr_reader :ranking
+
+        # @return [Integer]
+        attr_reader :rating
+
+        # @return [Integer]
+        attr_reader :season_games_played
+
+        # @return [Integer]
+        attr_reader :season_games_won
+
+        # @return [Integer]
+        attr_reader :last_season_ranking
+
+        # @return [Integer]
+        attr_reader :relevance
+
+        # @return [String]
+        attr_reader :url
+
+        # @return [Emblem]
+        attr_reader :emblem
+
         alias_method :to_s, :name
 
         def initialize(elem)
-          @name         = elem[:name]
-          @size         = elem[:size].to_i
-          @battle_group = elem[:battleGroup]
-          @faction      = elem[:faction]
-          @faction_id   = elem[:factionId].to_i
-          @realm        = elem[:realm]
-          @realm_url    = elem[:realmUrl]
+          @name                = elem[:name]
+          @size                = elem[:size].to_i
+          @battle_group        = elem[:battleGroup]
+          @faction             = elem[:faction]
+          @faction_id          = elem[:factionId].to_i
+          @realm               = elem[:realm]
+          @realm_url           = elem[:realmUrl]
 
-          @games_played = elem[:gamesPlayed].to_i
-          @games_won    = elem[:gamesWon].to_i
-          # @ranking      = elem[:ranking].to_i # Ranking in the seach results is always 0
-          @rating       = elem[:rating].to_i
+          @games_played        = elem[:gamesPlayed].to_i
+          @games_won           = elem[:gamesWon].to_i
+          @rating              = elem[:rating].to_i
 
-          @season_games_played  = elem[:seasonGamesPlayed].to_i
-          @season_games_won     = elem[:seasonGamesWon].to_i
-          @last_season_ranking  = elem[:lastSeasonRanking].to_i
+          @season_games_played = elem[:seasonGamesPlayed].to_i
+          @season_games_won    = elem[:seasonGamesWon].to_i
+          @last_season_ranking = elem[:lastSeasonRanking].to_i
 
-          @relevance    = elem[:relevance].to_i
-          @url          = elem[:url]
+          @relevance           = elem[:relevance].to_i # TODO: Move to SearchResult (tsigo)
+          @url                 = elem[:url]
 
-          @emblem       = Wowr::Classes::ArenaTeamEmblem.new(elem%'emblem')
+          @emblem              = Wowr::Classes::ArenaTeamEmblem.new(elem%'emblem')
         end
       end
     end
--- spec/wowr/armory/arena_team/base_spec.rb
+++ spec/wowr/armory/arena_team/base_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 module Wowr::Armory::ArenaTeam
   describe Base do
-    it { pending }
+    subject { Base.new(fake_element) }
+
+    its(:name)                { should eql("LEMON") }
+    its(:size)                { should eql(2) }
+    its(:battle_group)        { should eql("Cyclone") }
+    its(:faction)             { should eql("Horde") }
+    its(:faction_id)          { should eql(1) }
+    its(:realm)               { should eql("Stormscale") }
+    its(:realm_url)           { should match(/b=Cyclone.+r=Stormscale.+ts=2.+select=LEMON/) }
+    its(:games_played)        { should eql(0) }
+    its(:games_won)           { should eql(0) }
+    its(:rating)              { should eql(0) }
+    its(:season_games_played) { should eql(0) }
+    its(:season_games_won)    { should eql(0) }
+    its(:last_season_ranking) { should eql(0) }
+    its(:relevance)           { should_not be_nil }
+    its(:url)                 { should match(/r=Stormscale.+ts=2.+t=LEMON/) }
+    its(:emblem)              { should be_kind_of(Emblem) }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/search/arena_teams_lemon.xml')
+      (Hpricot.XML(xml).search("arenaTeam[@battleGroup='Cyclone']").first)
+    end
   end
 end
