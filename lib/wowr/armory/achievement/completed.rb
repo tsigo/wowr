ed9d6caaef0135cb2232b94b7ca15050c80ce5e5
rspeicher <rspeicher@gmail.com>
2010-11-09 15:53:33 -0500

    Spec Achievement::Completed

--- lib/wowr/armory/achievement/completed.rb
+++ lib/wowr/armory/achievement/completed.rb
@@ -1,15 +1,30 @@
 module Wowr
   module Armory
     module Achievement
-      class Completed < Wowr::Armory::Achievement::Base
+      # = Completed
+      #
+      # Represents a <tt>achievement</tt> element with a <tt>dateCompleted</tt> attribute
+      #
+      # == Relevant XML example:
+      #
+      #   <achievement categoryId="168" dateCompleted="2010-07-07T21:36:00-05:00"
+      #     desc="Defeat Halion in The Ruby Sanctum in 10-player mode." icon="spell_shadow_twilight"
+      #     id="4817" points="10" title="The Twilight Destroyer (10 player)"/>
+      #
+      # @todo I think we should just roll this class into "Base" with a <tt>completed?</tt> method.
+      class Completed < Base
+        # @param [String|DateTime]
         attr_reader :date_completed
+
+        # @param [Hpricot::Elem] achievement <tt>achievement</tt> element
         def initialize achievement
           super(achievement)
-          @date_completed = achievement['dateCompleted']
+
+          # Attempt to parse dateCompleted into a DateTime
           begin
-            @date_completed   = achievement[:dateCompleted] == "" ? nil : DateTime.parse(achievement[:dateCompleted])
-          rescue
-            @date_completed   = achievement[:dateCompleted] == "" ? nil : achievement[:dateCompleted]
+            @date_completed = DateTime.parse(achievement[:dateCompleted].to_s)
+          rescue ArgumentError
+            @date_completed = achievement[:dateCompleted].to_s
           end
         end
       end
--- spec/wowr/armory/achievement/completed_spec.rb
+++ spec/wowr/armory/achievement/completed_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 module Wowr::Armory::Achievement
   describe Completed do
-    it { pending }
+    context "successful DateTime.parse" do
+      subject { Completed.new(fake_element) }
+
+      its(:class)          { should superclass(Base) }
+      its(:date_completed) { should be_kind_of(DateTime) }
+    end
+
+    context "unsuccessful DateTime.parse" do
+      before  { DateTime.expects(:parse).raises(ArgumentError) }
+      subject { Completed.new(fake_element) }
+
+      its(:class)          { should superclass(Base) }
+      its(:date_completed) { should be_kind_of(String) }
+    end
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-achievements/sebudai_mal_ganis.xml')
+      (Hpricot.XML(xml)%'summary'/'achievement')[0]
+    end
   end
 end
