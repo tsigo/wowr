540bf2eeab6f60c830d55ad65d8d1c3c95688b62
rspeicher <rspeicher@gmail.com>
2010-11-09 17:52:23 -0500

    Spec Achievement::Category

--- lib/wowr/armory/achievement/category.rb
+++ lib/wowr/armory/achievement/category.rb
@@ -1,20 +1,36 @@
 module Wowr
   module Armory
     module Achievement
+      # = Category
+      #
+      # Represents a <tt>category</tt> element
+      #
+      # == Relevant XML example:
+      #
+      #   <category id="96" name="Quests">
       class Category
+        # @return [String]
         attr_reader :name
+
+        # @return [Array]
         attr_reader :subcategories
+
+        # @return [Integer]
         attr_reader :id
-        attr_writer :parent
-        def initialize category
-          @name = category['name']
-          @id = category['id'].to_i if category['id']
-          @subcategories = Array.new
+
+        # @return [Category]
+        attr_accessor :parent
+
+        # @param [Hpricot::Elem] elem <tt>category</tt> element
+        def initialize(elem)
+          @name          = elem[:name]
+          @id            = elem[:id].to_i if elem[:id]
+          @subcategories = []
         end
 
-        def add_subcategory category
-          @subcategories << category
+        def add_subcategory(category)
           category.parent = self
+          @subcategories << category
         end
       end
     end
--- spec/wowr/armory/achievement/category_spec.rb
+++ spec/wowr/armory/achievement/category_spec.rb
@@ -2,6 +2,31 @@ require 'spec_helper'
 
 module Wowr::Armory::Achievement
   describe Category do
-    it { pending }
+    subject { Category.new(fake_element) }
+
+    its(:name)          { should eql("Quests") }
+    its(:id)            { should eql(96) }
+    its(:parent)        { should be_nil }
+    its(:subcategories) { should eql([]) }
+
+    describe "add_subcategory" do
+      let(:sub) { (fake_element.search('category').first) }
+
+      it "should assign parent" do
+        expect { subject.add_subcategory(sub) }.to change(sub, :parent).to(subject)
+      end
+
+      it "should add to subcategories" do
+        subject.add_subcategory(sub)
+        subject.subcategories.should eql([sub])
+      end
+    end
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-achievements/sebudai_mal_ganis.xml')
+      (Hpricot.XML(xml).search("category[@id=96]").first)
+    end
   end
 end
