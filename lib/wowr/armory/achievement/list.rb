3e9a7601f2087a9ec5d19de633f9612a052dcbf2
rspeicher <rspeicher@gmail.com>
2010-11-09 16:35:52 -0500

    Spec Achievement::List

--- lib/wowr/armory/achievement/list.rb
+++ lib/wowr/armory/achievement/list.rb
@@ -1,19 +1,41 @@
 module Wowr
   module Armory
     module Achievement
+      # = List
+      #
+      # Represents a full <tt>achievements</tt> element
+      #
+      # == Relevant XML example:
+      #
+      #   <achievements lang="en_us" requestUrl="/character-achievements.xml">
+      #     <category>
+      #       <achievement categoryId="81" dateCompleted="2010-03-23T18:42:00-05:00" desc="Earn any variety of Emblem." icon="inv_misc_frostemblem_01" id="4785" title="Emblematic"/>
+      #       <achievement categoryId="81" dateCompleted="2010-03-15T21:07:00-05:00" desc="In the Trial of the Grand Crusader, reach a Tribute Chest with 50 attempts remaining and without allowing any raid member to die during any of the boss encounters in 25-player mode." icon="achievement_reputation_argentchampion" id="4079" reward="Reward: Crusader's Black Warhorse" title="A Tribute to Immortality">
+      #         <criteria id="12358" name="Complete the Trial of the Grand Crusader"/>
+      #       </achievement>
+      #       <achievement categoryId="81" dateCompleted="2009-09-23T22:14:00-05:00" desc="Participated in the realm first conquest of the Trial of the Grand Crusader with 50 attempts remaining in 25-player mode." icon="achievement_reputation_argentcrusader" id="4078" reward="Title Reward: The Grand Crusader" title="Realm First! Grand Crusader">
+      #         <criteria id="12350" name="Complete the Trial of the Grand Crusader"/>
+      #       </achievement>
+      #     </category>
+      #   </achievements>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/character-achievements.xml?r=Mal'Ganis&cn=Sebudai&gn=Juggernaut&c=81
+      # * http://www.wowarmory.com/character-achievements.xml?r=Mal'Ganis&cn=Sebudai&gn=Juggernaut&c=168
       class List
+        # @return [Array]
         attr_reader :achievements
-        def initialize elem, api
-          @api = api
+
+        # @param [Hpricot::Elem] elem <tt>achievements</tt> element
+        def initialize(elem)
           @achievements = Array.new
           elem.search('achievement').each do |achievement|
             if achievement['dateCompleted']
-              new_achievement = Wowr::Classes::CompletedAchievement.new(achievement)
+              @achievements << Wowr::Classes::CompletedAchievement.new(achievement)
             else
-              new_achievement = Wowr::Classes::Achievement.new(achievement)
+              @achievements << Wowr::Classes::Achievement.new(achievement)
             end
-            @achievements << new_achievement
-
           end
         end
       end
--- spec/wowr/armory/achievement/list_spec.rb
+++ spec/wowr/armory/achievement/list_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 module Wowr::Armory::Achievement
   describe List do
-    it { pending }
+    subject { List.new(fake_element) }
+
+    describe "achievements" do
+      it "should be an Array of [Completed]Achievement records" do
+        subject.achievements.should be_kind_of(Array)
+        subject.achievements[0].should be_kind_of(Base)
+      end
+    end
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-achievements/sebudai_mal_ganis_c_81.xml')
+      (Hpricot.XML(xml))
+    end
   end
 end
