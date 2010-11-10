b1d519b17b3cc6be2644c1a1eb98dad6fbc9e768
rspeicher <rspeicher@gmail.com>
2010-11-10 17:56:20 -0500

    Item::Info refactor
    Refactors a nice chunk of absolutely confusing code into something much nicer

--- lib/wowr/armory/item/info.rb
+++ lib/wowr/armory/item/info.rb
@@ -120,35 +120,12 @@ module Wowr
           # @costs << ItemCost.new(cost)
           #end
 
-          etc = [
-            # xml element name,   member variable         item list class name,   requires api link
-            ['disenchantLoot',    '@disenchant_items',    'item',     Wowr::Classes::DisenchantItem, true],
-            ['objectiveOfQuests', '@objective_of_quests', 'quest',    Wowr::Classes::ItemQuest, false],
-            ['rewardFromQuests',  '@reward_from_quests',  'quest',    Wowr::Classes::ItemQuest, false],
-            ['vendors',           '@vendors',             'creature', Wowr::Classes::ItemVendor, false],
-            ['dropCreatures',     '@drop_creatures',      'creature', Wowr::Classes::ItemDropCreature, false],
-            ['plansFor',          '@plans_for',           'spell',    Wowr::Classes::ItemPlansFor, true],
-          ]
-
-          etc.each do |b|
-            ele = b[0]
-            var = b[1]
-            list = b[2]
-            my_class = b[3]
-            requires_api = b[4]
-
-            if elem%ele
-              tmp_arr = []
-              (elem%ele/list).each do |x|
-                if requires_api
-                  tmp_arr << my_class.new(x, api)
-                else
-                  tmp_arr << my_class.new(x)
-                end
-              end
-              self.instance_variable_set(var, tmp_arr)
-            end
-          end
+          @disenchant_items    = extra_element(elem, :xml => 'disenchantLoot',    :list => 'item',     :class => 'DisenchantItem', :api => api)
+          @objective_of_quests = extra_element(elem, :xml => 'objectiveOfQuests', :list => 'quest',    :class => 'ItemQuest')
+          @reward_from_quests  = extra_element(elem, :xml => 'rewardFromQuests',  :list => 'quest',    :class => 'ItemQuest')
+          @vendors             = extra_element(elem, :xml => 'vendors',           :list => 'creature', :class => 'ItemDropCreature')
+          @drop_creatures      = extra_element(elem, :xml => 'dropCreatures',     :list => 'creature', :class => 'ItemDropCreature')
+          @plans_for           = extra_element(elem, :xml => 'plansFor',          :list => 'spell',    :class => 'ItemPlansFor', :api => api)
 
           # Rest of disenchant contents is done in the method above
           if (elem%'disenchantLoot')
@@ -163,6 +140,30 @@ module Wowr
           #   <createdBy>
           #     <spell icon="trade_engineering" id="60531" name="Create Dark Matter"/>
           #   </createdBy>
+        end
+
+        private
+
+        def extra_element(elem, options)
+          retval = []
+
+          # Check if the xml element exists
+          if (elem%options[:xml])
+            # The class that will be instantiated
+            klass = Wowr::Classes.const_get(options[:class])
+
+            # Loop through the list elements (e.g., 'disenchantLoot/item'),
+            # adding an instance of the specified class to the return value
+            (elem%options[:xml]/options[:list]).each do |list|
+              if options[:api]
+                retval << klass.new(list, options[:api])
+              else
+                retval << klass.new(list)
+              end
+            end
+          end
+
+          retval
         end
       end
     end
