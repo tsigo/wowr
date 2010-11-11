16e8a1f292ea7c6ed372bacb0a3be869481c17b9
rspeicher <rspeicher@gmail.com>
2010-11-11 15:31:12 -0500

    Spec Character::Spell::ManaRegen

--- lib/wowr/armory/character/spell/mana_regen.rb
+++ lib/wowr/armory/character/spell/mana_regen.rb
@@ -2,9 +2,23 @@ module Wowr
   module Armory
     module Character
       module Spell
+        # = ManaRegen
+        #
+        # Represents a <tt>spell/manaRegen</tt> element
+        #
+        # == Relevant XML example:
+        #
+        #   <manaRegen casting="193.00" notCasting="364.00"/>
         class ManaRegen
-          attr_reader :casting, :not_casting
+          # Regen while casting
+          # @return [Float]
+          attr_reader :casting
 
+          # Regen while not casting
+          # @return [Float]
+          attr_reader :not_casting
+
+          # @param [Hpricot::Elem] elem <tt>spell/manaRegen</tt> element
           def initialize(elem)
             @casting      = elem[:casting].to_f
             @not_casting  = elem[:notCasting].to_f
--- spec/wowr/armory/character/spell/mana_regen_spec.rb
+++ spec/wowr/armory/character/spell/mana_regen_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 module Wowr::Armory::Character::Spell
   describe ManaRegen do
+    subject { ManaRegen.new(fake_element) }
 
-    it { pending }
+    its(:casting)     { should satisfy { |v| v > 0.00 } }
+    its(:not_casting) { should satisfy { |v| v > 0.00 } }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-sheet/fearsom_mal_ganis.xml')
+      (Hpricot.XML(xml)%'spell'/'manaRegen')[0]
+    end
 
   end
 end
