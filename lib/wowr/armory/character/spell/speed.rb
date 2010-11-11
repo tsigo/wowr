59da53b3e1244eab132293fa1e91bb18b7892c11
rspeicher <rspeicher@gmail.com>
2010-11-11 15:38:41 -0500

    Spec Character::Spell::Speed

--- lib/wowr/armory/character/spell/speed.rb
+++ lib/wowr/armory/character/spell/speed.rb
@@ -2,12 +2,26 @@ module Wowr
   module Armory
     module Character
       module Spell
+        # = Speed
+        #
+        # Represents a <tt>hasteRating</tt> element
+        #
+        # == Relevant XML example:
+        #
+        #   <hasteRating hastePercent="43.37" hasteRating="1422"/>
         class Speed
-          attr_reader :percent_increase, :haste_rating
+          # @todo Check naming consistency
+          # @return [Float]
+          attr_reader :percent_increase
 
+          # @todo Check naming consistency
+          # @return [Integer]
+          attr_reader :haste_rating
+
+          # @param [Hpricot::Elem] elem <tt>hasteRating</tt> element
           def initialize(elem)
             @percent_increase = elem[:hastePercent].to_f
-            @haste_rating   = elem[:hasteRating].to_i
+            @haste_rating     = elem[:hasteRating].to_i
           end
         end
       end
--- spec/wowr/armory/character/spell/speed_spec.rb
+++ spec/wowr/armory/character/spell/speed_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 module Wowr::Armory::Character::Spell
   describe Speed do
+    subject { Speed.new(fake_element) }
 
-    it { pending }
+    its(:haste_rating)     { should satisfy { |v| v > 0 && v < 5000 } }
+    its(:percent_increase) { should satisfy { |v| v >= 0.00 && v <= 100.00 } }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-sheet/fearsom_mal_ganis.xml')
+      (Hpricot.XML(xml)%'spell'/'hasteRating')[0]
+    end
 
   end
 end
