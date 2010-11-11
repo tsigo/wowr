73fd44f0106bb8e4ed455e56d1bb116f8b224cd3
rspeicher <rspeicher@gmail.com>
2010-11-11 15:26:32 -0500

    Spec Character::Spell:Damage

--- lib/wowr/armory/character/spell/damage.rb
+++ lib/wowr/armory/character/spell/damage.rb
@@ -2,13 +2,45 @@ module Wowr
   module Armory
     module Character
       module Spell
+        # = Damage
+        #
+        # Combines <tt>spell/bonusDamage</tt> and <tt>spell/critChance</tt>
+        # elements for a single damage type into one representation
+        #
+        # == Relevant XML examples:
+        #
+        #   <bonusDamage>
+        #     <arcane value="3751"/>
+        #     <fire value="3751"/>
+        #     <frost value="3751"/>
+        #     <holy value="3751"/>
+        #     <nature value="3751"/>
+        #     <shadow value="3751"/>
+        #     <petBonus attack="2138" damage="563" fromType="fire"/>
+        #   </bonusDamage>
+        #
+        #   <critChance rating="530">
+        #     <arcane percent="26.34"/>
+        #     <fire percent="26.34"/>
+        #     <frost percent="26.34"/>
+        #     <holy percent="26.34"/>
+        #     <nature percent="26.34"/>
+        #     <shadow percent="26.34"/>
+        #   </critChance>
         class Damage
-          attr_reader :value, :crit_chance_percent
+          # @return [Integer]
+          attr_reader :value
+
+          # @return [Float]
+          attr_reader :crit_chance_percent
+
           alias_method :percent, :crit_chance_percent
 
+          # @param [Hpricot::Elem] bonusDamage_elem An element under <tt>spell/bonusDamage</tt>
+          # @param [Hpricot::Elem] critChance_elem An element under <tt>spell/critChance</tt>
           def initialize(bonusDamage_elem, critChance_elem)
-            @value    = bonusDamage_elem[:value].to_i
-            @crit_chance_percent  = critChance_elem[:percent].to_f
+            @value               = bonusDamage_elem[:value].to_i
+            @crit_chance_percent = critChance_elem[:percent].to_f
           end
         end
       end
--- spec/wowr/armory/character/spell/damage_spec.rb
+++ spec/wowr/armory/character/spell/damage_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 module Wowr::Armory::Character::Spell
   describe Damage do
+    subject { Damage.new(*fake_element) }
 
-    it { pending }
+    its(:value)               { should satisfy { |v| v > 3000 } }
+    its(:crit_chance_percent) { should satisfy { |v| v >= 0.00 && v <= 100.00 } }
+
+    # Aliased methods
+    its(:percent) { should_not be_nil }
+
+    protected
+
+    def fake_element
+      xml = file_fixture('armory/character-sheet/fearsom_mal_ganis.xml')
+      elem = (Hpricot.XML(xml)%'spell')
+
+      [elem%'bonusDamage'%'shadow', elem%'critChance'%'shadow']
+    end
 
   end
 end
