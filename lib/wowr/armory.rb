6fcee292189dafa05e0089c0a4ec37212a4554e5
rspeicher <rspeicher@gmail.com>
2010-11-10 22:06:44 -0500

    Swap 'extend self' in favor of 'class << self'
    Yard was listing the class methods as instance methods

--- lib/wowr/armory.rb
+++ lib/wowr/armory.rb
@@ -5,8 +5,6 @@ module Wowr
   #
   # Also provides a set of helper methods for dealing with Armory data
   module Armory
-    extend self
-
     # @see #damage_type
     DAMAGE_TYPES = [:physical, :holy, :fire, :nature, :frost, :shadow, :arcane].freeze
 
@@ -19,68 +17,70 @@ module Wowr
     # @see #quality
     QUALITIES = [:trash, :common, :uncommon, :rare, :epic, :legendary, :artifact, :heirloom].freeze
 
-    # Maps an integer-based damage type to its name
-    #
-    # @example
-    #   >> Armory.damage_type(3)
-    #   => :fire
-    #   >> Armory.damage_type(6)
-    #   => :arcane
-    # @raise [ArgumentError] Invalid damage type
-    # @param [Integer] type
-    # @return [Symbol]
-    def damage_type(type)
-      type = type.to_i
-      raise ArgumentError, 'invalid damage type' unless DAMAGE_TYPES[type]
-      DAMAGE_TYPES[type]
-    end
+    class << self
+      # Maps an integer-based damage type to its name
+      #
+      # @example
+      #   >> Armory.damage_type(3)
+      #   => :fire
+      #   >> Armory.damage_type(6)
+      #   => :arcane
+      # @raise [ArgumentError] Invalid damage type
+      # @param [Integer] type
+      # @return [Symbol]
+      def damage_type(type)
+        type = type.to_i
+        raise ArgumentError, 'invalid damage type' unless DAMAGE_TYPES[type]
+        DAMAGE_TYPES[type]
+      end
 
-    # Maps an integer-based faction ID to its name
-    #
-    # @example
-    #   >> Armory.faction(0)
-    #   => :alliance
-    #   >> Armory.faction(1)
-    #   => :horde
-    # @raise [ArgumentError] Invalid faction
-    # @param [Integer] id
-    # @return [Symbol]
-    def faction(id)
-      id = id.to_i
-      raise ArgumentError, 'invalid faction' unless FACTIONS[id]
-      FACTIONS[id]
-    end
+      # Maps an integer-based faction ID to its name
+      #
+      # @example
+      #   >> Armory.faction(0)
+      #   => :alliance
+      #   >> Armory.faction(1)
+      #   => :horde
+      # @raise [ArgumentError] Invalid faction
+      # @param [Integer] id
+      # @return [Symbol]
+      def faction(id)
+        id = id.to_i
+        raise ArgumentError, 'invalid faction' unless FACTIONS[id]
+        FACTIONS[id]
+      end
 
-    # Maps a single-letter power type to its full name
-    #
-    # @example
-    #   >> Armory.power_type('e')
-    #   => :energy
-    #   >> Armory.power_type(:m)
-    #   => :mana
-    # @raise [ArgumentError] Invalid power type
-    # @param [String, Symbol] type
-    # @return [Symbol]
-    def power_type(type)
-      type = type.to_sym if type.respond_to? :to_sym
-      raise ArgumentError, 'invalid power type' unless POWER_TYPES.keys.include? type
-      POWER_TYPES[type]
-    end
+      # Maps a single-letter power type to its full name
+      #
+      # @example
+      #   >> Armory.power_type('e')
+      #   => :energy
+      #   >> Armory.power_type(:m)
+      #   => :mana
+      # @raise [ArgumentError] Invalid power type
+      # @param [String, Symbol] type
+      # @return [Symbol]
+      def power_type(type)
+        type = type.to_sym if type.respond_to? :to_sym
+        raise ArgumentError, 'invalid power type' unless POWER_TYPES.keys.include? type
+        POWER_TYPES[type]
+      end
 
-    # Maps an integer-based quality value to its name
-    #
-    # @example
-    #   >> Armory.quality(4)
-    #   => :epic
-    #   >> Armory.quality(0)
-    #   => :trash
-    # @raise [ArgumentError] Invalid quality
-    # @param [Integer] id
-    # @return [Symbol]
-    def quality(id)
-      id = id.to_i
-      raise ArgumentError, 'invalid quality value' unless QUALITIES[id]
-      QUALITIES[id]
+      # Maps an integer-based quality value to its name
+      #
+      # @example
+      #   >> Armory.quality(4)
+      #   => :epic
+      #   >> Armory.quality(0)
+      #   => :trash
+      # @raise [ArgumentError] Invalid quality
+      # @param [Integer] id
+      # @return [Symbol]
+      def quality(id)
+        id = id.to_i
+        raise ArgumentError, 'invalid quality value' unless QUALITIES[id]
+        QUALITIES[id]
+      end
     end
   end
 end
