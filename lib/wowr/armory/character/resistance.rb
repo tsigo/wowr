28302c19086f1e76c84e77ccf96c8100ce15b7ee
rspeicher <rspeicher@gmail.com>
2010-11-10 21:10:41 -0500

    Spec Character::Resistance

--- lib/wowr/armory/character/resistance.rb
+++ lib/wowr/armory/character/resistance.rb
@@ -1,9 +1,35 @@
 module Wowr
   module Armory
     module Character
+      # = Resistance
+      #
+      # Represents one of the elements in the <tt>resistances</tt> element
+      #
+      # == Relevant XML examples:
+      #
+      #   <arcane petBonus="0" value="0"/>
+      #
+      #   <fire petBonus="0" value="0"/>
+      #
+      #   <frost petBonus="0" value="0"/>
+      #
+      #   <holy petBonus="0" value="0"/>
+      #
+      #   <nature petBonus="0" value="0"/>
+      #
+      #   <shadow petBonus="0" value="0"/>
+      #
+      # == Example Pages:
+      #
+      # * http://www.wowarmory.com/character-sheet.xml?r=Mal'Ganis&cn=Sebudai
       class Resistance
-        attr_reader :value, :pet_bonus
+        # @return [Integer]
+        attr_reader :value
 
+        # @return [Integer]
+        attr_reader :pet_bonus
+
+        # @param [Hpricot::Elem] elem An element under <tt>resistances</tt>
         def initialize(elem)
           @value      = elem[:value].to_i
           @pet_bonus  = elem[:petBonus].to_i == -1 ? nil : elem[:petBonus].to_i
--- spec/wowr/armory/character/resistance_spec.rb
+++ spec/wowr/armory/character/resistance_spec.rb
@@ -2,8 +2,26 @@ require 'spec_helper'
 
 module Wowr::Armory::Character
   describe Resistance do
+    subject { Resistance.new(fake_element) }
 
-    it { pending }
+    its(:value)     { should satisfy { |v| v >= 100 } }
+    its(:pet_bonus) { should satisfy { |v| v >= 200 } }
+
+    protected
+
+    def fake_element
+      # NOTE: It's hard to catch anyone on the Armory in resistance gear, so
+      # we're going to fake it by using a hash, just this once. But we're still
+      # using a fixture to catch any changes to the Armory attributes.
+
+      xml = file_fixture('armory/character-sheet/sebudai_mal_ganis.xml')
+      elem = (Hpricot.XML(xml)%'resistances'/'shadow')[0]
+
+      elem[:value].should_not be_nil
+      elem[:petBonus].should_not be_nil
+
+      {:value => (elem[:value].to_i + 100).to_s, :petBonus => (elem[:petBonus].to_i + 200).to_s}
+    end
 
   end
 end
