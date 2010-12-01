9429ed435653bfd4a864ecba6ca6569de457c074
Lloyd Pick <lloydpick@gmail.com>
2010-12-01 17:16:33 +0000

    added glyph support and tests for talent specs

--- lib/wowr/armory/character/talent_spec.rb
+++ lib/wowr/armory/character/talent_spec.rb
@@ -9,10 +9,52 @@ module Wowr
       #
       #   <talentGroup active="1" group="2" icon="ability_hunter_focusedaim" prim="Marksmanship">
       #     <talentSpec treeOne="0" treeThree="5" treeTwo="31" value="0000000000000000000230231103212023122103200000000000000000"/>
-      #     <glyphs/>
+      #     <glyphs>
+      #       <glyph effect="Reduces the focus cost of Trap Launcher by 10." icon="ability_hunter_traplauncher" id="353" name="Glyph of Trap Launcher" type="major"/>
+      #       <glyph effect="Increases the range of your Aspect of the Pack ability by 15 yards." icon="ability_mount_whitetiger" id="443" name="Glyph of Aspect of the Pack" type="minor"/>
+      #       <glyph effect="Your Mend Pet spell increases your pet's happiness slightly." icon="ability_hunter_mendpet" id="440" name="Glyph of Mend Pet" type="minor"/>
+      #       <glyph effect="Decreases the cooldown of Disengage by 5 sec." icon="ability_rogue_feint" id="358" name="Glyph of Disengage" type="major"/>
+      #       <glyph effect="Reduces the cooldown of your Feign Death spell by 5 sec." icon="ability_rogue_feigndeath" id="441" name="Glyph of Feign Death" type="minor"/>
+      #       <glyph effect="Increases the total amount of healing done by your Mend Pet ability by 60%." icon="ability_hunter_mendpet" id="354" name="Glyph of Mending" type="major"/>
+      #       <glyph effect="Increases the damage dealt by Steady Shot by 10%." icon="ability_hunter_steadyshot" id="368" name="Glyph of Steady Shot" type="prime"/>
+      #       <glyph effect="Your Arcane Shot deals 12% more damage." icon="ability_impalingbolt" id="352" name="Glyph of Arcane Shot" type="prime"/>
+      #       <glyph effect="If the damage from your Kill Shot fails to kill a target at or below 20% health, your Kill Shot's cooldown is instantly reset. This effect has a 6 sec cooldown." icon="ability_hunter_assassinate2" id="692" name="Glyph of Kill Shot" type="prime"/>
+      #     </glyphs>
       #   </talentGroup>
       #
       # @todo Rename? It's a Talent Group, not just a Talent Spec
+      class Glyph
+        # The item ID for the particular glyph
+        # @return [Integer]
+        attr_reader :id
+
+        # The specified effect of the glyph
+        # @return [String]
+        attr_reader :effect
+
+        # The name of the glyph
+        # @return [String]
+        attr_reader :name
+
+        # The type of glyph, either "Minor", "Major", or "Prime"
+        # @return [String]
+        attr_reader :type
+
+        # The icon for this glyph
+        # @return [String]
+        attr_reader :icon
+
+        # @param [Nokogiri::XML::Element] elem <tt>glyph</tt> element
+        def initialize(elem)
+          @id = elem[:id].to_i
+          @effect = elem[:effect]
+          @name = elem[:name]
+          @type = elem[:type]
+          @icon = elem[:icon]
+        end
+      end
+
+
       class TalentSpec
         # An array containing the total points in each tree.
         # @note Tree one is in array index 0, two is in 1, and three is in 2
@@ -41,6 +83,10 @@ module Wowr
         # @return [String]
         attr_reader :point_distribution
 
+        # An array containing the glyphs in each tree.
+        # @return [Array]
+        attr_reader :glyphs
+
         # @param [Nokogiri::XML::Element] elem <tt>talentGroup</tt> element
         def initialize(elem)
           tree_elem           = elem%'talentSpec'
@@ -52,6 +98,12 @@ module Wowr
           @group              = elem[:group].to_i
           @primary            = elem[:prim]
           @point_distribution = tree_elem[:value]
+
+          @glyphs             = []
+          glyph_elem          = elem%'glyphs'
+          glyph_elem.elements.each do |glyph|
+            @glyphs << Glyph.new(glyph)
+          end
         end
       end
     end
--- spec/wowr/armory/character/talent_spec_spec.rb
+++ spec/wowr/armory/character/talent_spec_spec.rb
@@ -8,6 +8,22 @@ module Wowr::Armory::Character
     its(:group) { should eql(2) }
     its(:primary) { should eql("Beast Mastery") }
     its(:point_distribution) { should match(/^\d+$/) }
+    its(:trees) { should be_kind_of(Array) }
+
+    describe "glyphs" do
+      it "should return an Array of Glyphs" do
+        subject.glyphs.should be_kind_of(Array)
+        subject.glyphs[0].should be_kind_of(Glyph)
+      end
+
+      it "should return the expected values" do
+        subject.glyphs[0].id.should eql(353)
+        subject.glyphs[0].effect.should eql("Reduces the focus cost of Trap Launcher by 10.")
+        subject.glyphs[0].name.should eql("Glyph of Trap Launcher")
+        subject.glyphs[0].type.should eql("major")
+        subject.glyphs[0].icon.should eql("ability_hunter_traplauncher")
+      end
+    end
 
     protected
 
