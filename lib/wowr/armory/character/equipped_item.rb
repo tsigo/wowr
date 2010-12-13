11ffa75e9e80e5a4b7c65d368ec8347fff53a7e1
rspeicher <rspeicher@gmail.com>
2010-12-13 15:30:53 -0500

    Character::EquippedItem changes
    MaxDurability and PermanentEnchantId have apparently been removed from
    the Armory XML.

--- lib/wowr/armory/character/equipped_item.rb
+++ lib/wowr/armory/character/equipped_item.rb
@@ -7,19 +7,16 @@ module Wowr
       #
       # == Relevant XML examples:
       #
-      #   <item displayInfoId="65131" durability="100" gem0Id="41398" gem1Id="40112" gem2Id="0" gemIcon0="inv_jewelcrafting_shadowspirit_02" gemIcon1="inv_jewelcrafting_gem_37" icon="inv_helmet_158" id="51286" level="277" maxDurability="100" name="Sanctified Ahn'Kahar Blood Hunter's Headpiece" permanentEnchantIcon="ability_warrior_rampage" permanentEnchantItemId="44149" permanentenchant="3817" pickUp="PickUpSmallChain" putDown="PutDownSmallChain" randomPropertiesId="0" rarity="4" seed="0" slot="0"/>
+      #   <item durability="45" gem0Id="41398" gem1Id="52220" gem2Id="0" id="58123" permanentenchant="4209" pickUp="" putDown="" randomPropertiesId="0" seed="0" slot="0"/>
       #
-      #   <item displayInfoId="64216" durability="0" gem0Id="40157" gem1Id="0" gem2Id="0" gemIcon0="inv_jewelcrafting_gem_40" icon="inv_misc_monsterhorn_03" id="50633" level="277" maxDurability="0" name="Sindragosa's Cruel Claw" permanentenchant="0" pickUp="PickUpRing" putDown="PutDownRing" randomPropertiesId="0" rarity="4" seed="1348559296" slot="1"/>
+      #   <item durability="0" gem0Id="0" gem1Id="0" gem2Id="0" id="62378" permanentenchant="0" pickUp="" putDown="" randomPropertiesId="0" seed="0" slot="1"/>
       #
-      #   <item displayInfoId="64829" durability="100" gem0Id="40112" gem1Id="0" gem2Id="0" gemIcon0="inv_jewelcrafting_gem_37" icon="inv_shoulder_113" id="51288" level="277" maxDurability="100" name="Sanctified Ahn'Kahar Blood Hunter's Spaulders" permanentEnchantIcon="inv_axe_85" permanentEnchantItemId="44871" permanentenchant="3808" pickUp="PickUpSmallChain" putDown="PutDownSmallChain" randomPropertiesId="0" rarity="4" seed="0" slot="2"/>
+      #   <item durability="42" gem0Id="52219" gem1Id="0" gem2Id="0" id="58124" permanentenchant="4205" pickUp="" putDown="" randomPropertiesId="0" seed="0" slot="2"/>
       #
-      #   <item displayInfoId="64840" durability="165" gem0Id="40112" gem1Id="40157" gem2Id="0" gemIcon0="inv_jewelcrafting_gem_37" gemIcon1="inv_jewelcrafting_gem_40" icon="inv_chest_mail_11" id="51289" level="277" maxDurability="165" name="Sanctified Ahn'Kahar Blood Hunter's Tunic" permanentEnchantIcon="inv_scroll_03" permanentEnchantItemId="44465" permanentenchant="3832" pickUp="PickUpSmallChain" putDown="PutDownSmallChain" randomPropertiesId="0" rarity="4" seed="0" slot="4"/>
+      #   <item durability="94" gem0Id="52212" gem1Id="52220" gem2Id="0" id="59119" permanentenchant="3832" pickUp="" putDown="" randomPropertiesId="0" seed="0" slot="4"/>
       class EquippedItem < Wowr::Armory::Item::Base
         # @return [Integer]
         attr_reader :durability
-
-        # @return [Integer]
-        attr_reader :max_durability
 
         # @return [Array] Array of Gem IDs
         attr_reader :gems
@@ -28,10 +25,6 @@ module Wowr
         # @note This is not the ID of the item that provides the enchant
         # @return [Integer]
         attr_reader :permanent_enchant
-
-        # The ID of the item providing the enchant
-        # @return [Integer]
-        attr_reader :permanent_enchant_item_id
 
         # @return [Integer]
         attr_reader :random_properties_id
@@ -52,7 +45,6 @@ module Wowr
           @gems[1]                   = elem[:gem1Id].to_i == 0 ? nil : elem[:gem1Id].to_i
           @gems[2]                   = elem[:gem2Id].to_i == 0 ? nil : elem[:gem2Id].to_i
           @permanent_enchant         = elem[:permanentenchant].to_i
-          @permanent_enchant_item_id = elem[:permanentEnchantItemId].to_i
           @random_properties_id      = elem[:randomPropertiesId] == 0 ? nil : elem[:randomPropertiesId].to_i
           @seed                      = elem[:seed].to_i # not sure if seed is so big it's overloading
           @slot                      = elem[:slot].to_i
--- spec/wowr/armory/character/equipped_item_spec.rb
+++ spec/wowr/armory/character/equipped_item_spec.rb
@@ -5,13 +5,11 @@ module Wowr::Armory::Character
     subject { EquippedItem.new(fake_element) }
 
     its(:durability)                { should satisfy { |v| v > 0 && v <= 100 } }
-    its(:max_durability)            { should eql(100) }
     its(:gems)                      { should be_kind_of(Array) }
-    its(:gems)                      { subject[0].should eql(41307) }
-    its(:gems)                      { subject[1].should eql(40112) }
+    its(:gems)                      { subject[0].should_not be_nil }
+    its(:gems)                      { subject[1].should_not be_nil }
     its(:gems)                      { subject[2].should be_nil }
-    its(:permanent_enchant)         { should eql(3817) }
-    its(:permanent_enchant_item_id) { should eql(44149) }
+    its(:permanent_enchant)         { should_not be_nil }
     its(:random_properties_id)      { should eql(0) }
     its(:slot)                      { should eql(0) }
 
